20b09c2b9f30af006abb7896088a9217e779def0
Nay Min Lwin <nayminlwin@gmail.com>
2023-07-04 23:38:56 +0630

    Cleared old files and tried to fix pathing issue

--- internal/immutable/chunks/0-eaa10265.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/layout.svelte-229c5288.js";export{t as component};
--- internal/immutable/chunks/1-11380396.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-35715d80.js";export{t as component};
