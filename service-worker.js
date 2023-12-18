5c87a740f8f2a8c5b1f81c6d204fae57be818bc7
Nay Min Lwin <nayminlwin@gmail.com>
2023-12-18 23:39:34 +0630

    Fixed minor bug

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
 const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), o = [
-  e + "/internal/immutable/entry/app.1f240f4f.js",
+  e + "/internal/immutable/entry/app.78e0799b.js",
   e + "/internal/immutable/nodes/0.ee9d3fc1.js",
-  e + "/internal/immutable/nodes/1.ee8a5a5e.js",
+  e + "/internal/immutable/nodes/1.ed0ff4db.js",
   e + "/internal/immutable/assets/2.72972c6e.css",
-  e + "/internal/immutable/nodes/2.548f6d45.js",
+  e + "/internal/immutable/nodes/2.c2230394.js",
   e + "/internal/immutable/chunks/index.4dfd544e.js",
-  e + "/internal/immutable/chunks/paths.3090b573.js",
+  e + "/internal/immutable/chunks/paths.59e3d48c.js",
   e + "/internal/immutable/chunks/scheduler.1d0e75b3.js",
-  e + "/internal/immutable/chunks/singletons.7a5a9665.js",
-  e + "/internal/immutable/entry/start.3d74f1e0.js"
+  e + "/internal/immutable/chunks/singletons.03d3b0ce.js",
+  e + "/internal/immutable/entry/start.e92f3352.js"
 ], r = [
   e + "/letkwet.ico",
   e + "/letkwet.svg",
   e + "/songs.json"
-], u = "1702917989350", i = `cache-${u}`, l = [
+], u = "1702919298823", i = `cache-${u}`, l = [
   ...o,
   // the app itself
   ...r
